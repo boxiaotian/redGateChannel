b0c65dcff0c3cda80b39cdd0b4981dc19fa2fb0a
XPgan <1229308017@qq.com>
2020-04-11 10:18:02 +0800

    优化环境配置

--- src/constants/counter.js
+++ src/constants/counter.js
@@ -1,9 +1,32 @@
 export const ADD = 'ADD'
 export const MINUS = 'MINUS'
 //http://192.168.0.105:10086/  https://app.hongmenpd.com/H5/#/  https://hm.hongmenpd.com/H5/#/
-export const url_domain = ' https://app.hongmenpd.com/H5/#/'
-export const image_domain = 'https://qiniu.hongmenpd.com/H5/images/'
-export const formal_domain = 'https://app.hongmenpd.com/api/'  // https://app.hongmenpd.com/api/
+const evnList = {
+  dev: { 
+    url_domain:'https://hm.hongmenpd.com/H5/#/',
+    formal_domain:'https://hm.hongmenpd.com/api/',
+    image_domain: 'https://qiniu.hongmenpd.com/H5/images/'
+  },
+  prod: {
+    url_domain:'https://app.hongmenpd.com/H5/#/',
+    formal_domain : 'https://app.hongmenpd.com/api/',
+    image_domain: 'https://qiniu.hongmenpd.com/H5/images/'
+  },
+  test: { 
+    url_domain:'https://hm.hongmenpd.com/H5/#/',
+    formal_domain:'https://hm.hongmenpd.com/api/',
+    image_domain: 'https://qiniu.hongmenpd.com/H5/images/'
+  }
+}
+let params = {
+  'hm.hongmenpd.com': 'dev',
+  'app.hongmenpd.com': 'prod',
+  '192.168.1.73': 'test', //本机调试修改此处
+}
+let currentEnv = params[location.hostname]
+export const url_domain = evnList[currentEnv].url_domain
+export const image_domain = evnList[currentEnv].image_domain
+export const formal_domain = evnList[currentEnv].formal_domain
 export const package_privilege = [{  // 礼包特权
   image: image_domain + 'gift1.png',
   value: '先行赔付'
@@ -49,13 +72,13 @@ export const share_icon = image_domain + 'share_icon.png';
 export const my_key = [{
   image: image_domain + 'effort.png',
   value: '收益'
-},{
+}, {
   image: image_domain + 'order.png',
   value: '订单'
-},{
+}, {
   image: image_domain + 'follower.png',
   value: '粉丝'
-},{
+}, {
   image: image_domain + 'card.png',
   value: '卡券'
 }]
